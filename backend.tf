861cd4ac40e5bb546f4c7a9ac0acde539cb2298a
megha-kharade <91042594+megha-kharade@users.noreply.github.com>
2022-04-13 15:49:51 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "as1506-terraform-state"
     key    = "s3-state-file"
     region = "ap-south-1"
+    access_key = "AKIATDMUNIQBNE6QYR4V" 
+    secret_key = "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"
   }
 }
